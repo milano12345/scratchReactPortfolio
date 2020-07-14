6645e2dc0092b06bd138ca4b944069ffd46e5338
milano12345 <dmilano857@gmail.com>
2020-07-14 15:52:01 -0600

    fixed css to be responsive

--- scratch-portfolio/src/components/footer.js
+++ scratch-portfolio/src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p style={{ marginTop: "2rem" }}>&copy; Daniel Milano Design</p>
+      <p>&copy; Daniel Milano Design</p>
     </footer>
   );
 };
